246e59f20e293641a05301acbb59e71ac3f6f1af
Yann <martinez.yann@outlook.fr>
2025-06-30 15:04:51 +0200

    mise à jour

--- routes.py
+++ routes.py
@@ -1,4 +1,5 @@
 from flask import render_template, redirect, url_for, session, flash, request, jsonify
+import tempfile
 import os
 from werkzeug.utils import secure_filename
 from agents.utils.import_connectors import (
