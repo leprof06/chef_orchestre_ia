4a85f71a6bbebba2a2f5a3bcca8a6fab9addf5c7
Yann <martinez.yann@outlook.fr>
2025-06-30 18:29:08 +0200

    mise à jour

--- app.py
+++ app.py
@@ -1,13 +1,21 @@
 from flask import Flask
 from orchestrator import Orchestrator
 from routes import register_routes
+import os
 
 app = Flask(__name__)
 app.secret_key = "dev-secret-1234"
 orchestrator = Orchestrator()
 
+# CHEMIN où tu veux stocker les uploads
+UPLOAD_FOLDER = os.path.join(os.path.dirname(__file__), "uploads")
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
 # Plug toutes les routes à l'app
 register_routes(app, orchestrator)
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
